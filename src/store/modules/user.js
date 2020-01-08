0ad522fe2491915e3bd2419856cbeda2f595454c
Wai Lun <wailun@u.nus.edu>
2020-01-08 18:42:58 +0800

    Update logic for when buying stock

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -5,7 +5,14 @@ export default {
   },
   mutations: {
     addToPortfolio(state, payload) {
-      state.userStocks.push(payload);
+      const index = state.userStocks.findIndex(stock => stock.info.name === payload.info.name);
+      if (index === -1) {
+        // adding a totally new stock
+        state.userStocks.push(payload);
+      } else {
+        // incrementing the quantity of the stock that user already has
+        state.userStocks[index].quantity += payload.quantity;
+      }
     },
     updateFunds(state, payload) {
       state.funds = payload;
@@ -15,7 +22,7 @@ export default {
     buyStock({ state, commit }, payload) {
       const cost = state.funds - payload.info.price * payload.quantity;
       if (cost < 0) {
-        // user cannot afford
+        // user cannot afford, do not commit
         return;
       }
       commit("updateFunds", cost);
